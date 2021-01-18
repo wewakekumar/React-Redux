116cefc560dd69aae4507f12029ecd8bfd328ea8
wewakekumargetpaz <vivek.kumar@getpaz.com>
2021-01-18 10:20:21 +0530

    react-redux added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import React from 'react';
+import {connect} from 'react-redux';
+const App=(props)=> {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Header">
+        <h1>Counter:{props.ctr}</h1>
+      </div>
+      <div>
+        <button>Increment</button>
+        <button>Decrement</button>
+        <button>Add</button>
+        <button>Subtract</button>
+      </div>
     </div>
   );
 }
 
-export default App;
+const mapStatesToProps=(state)=>{
+  return{ctr:state.counter}
+}
+
+export default connect(mapStatesToProps)(App);
---
+++ src/reducers/reducer.js
@@ -0,0 +1,9 @@
+const initialState={
+    counter:1
+}
+
+const reducer=(state=initialState,action)=>{
+    return state;
+}
+
+export default reducer;
